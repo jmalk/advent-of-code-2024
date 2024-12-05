a78b6b519259145627fa64c769aa78782c393028
Josh Malkinson <joshua.malkinson@coop.co.uk>
2024-12-05 20:24:12 +0000

    Inline redundant variables

--- day-03/solution.ts
+++ day-03/solution.ts
@@ -1,21 +1,19 @@
 export const read = (memory: string) => {
-  const ret = Array.from(
+  return Array.from(
     // Find all mul(xxx,xxx)
     memory.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g),
     // Map to tuples of [number, number]
     (m) => [parseInt(m[1]), parseInt(m[2])] as const,
   );
-  return ret;
 };
 
 export const readWithConditionals = (memory: string) => {
-  const ret = Array.from(
+  return Array.from(
     // Find all mul(xxx,xxx)
     memory.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g),
     // Map to tuples of [number, number]
     (m) => [parseInt(m[1]), parseInt(m[2])] as const,
   );
-  return ret;
 };
 
 export const multiply = (pairs: (readonly [number, number])[]) => {
